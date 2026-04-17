f00410c2840582ab2e176b41ddcd6275678141e9
Nikita Shokarev <14005509+nikitos4683@users.noreply.github.com>
2026-04-17 23:50:48 +0300

    chore: hide profile selection mentions

--- action.sh
+++ action.sh
@@ -26,7 +26,7 @@ is_running() {
 print_status_summary() {
     echo ""
     echo "[*] Current Status:"
-    "$CLI" status | grep -E 'nfqws2:|iptables|Network mode:|Active profile:'
+    "$CLI" status | grep -E 'nfqws2:|iptables|Network mode:'
 }
 
 print_header
